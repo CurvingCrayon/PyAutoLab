1063ce75ea407639217b1cac684d9319e2b6125c
Hayden Keers <hayden.keers@gmail.com>
2019-09-06 00:09:59 +1000

    Basic tab GUI

--- gui.py
+++ gui.py
@@ -1,6 +1,16 @@
 import tkinter
-mainWindow = tkinter.Tk() 
-mainWindow.title("PyAutoLab")
+from tkinter import ttk
+win = tkinter.Tk() 
+win.title("PyAutoLab")
 #create a title and display it with necessary space
-titleLb = tkinter.Label(mainWindow, text = "Hayden's PyAutoLab").pack() 
-mainWindow.mainloop()
+tkinter.Label(win, text = "Hayden's PyAutoLab").pack() 
+tabs = ttk.Notebook()
+#Create main execution tab
+runTab = ttk.Frame(tabs)
+tabs.add(runTab, text="Run")
+tabs.pack(expand=1, fill="both")
+#Create setup tab
+setupTab = ttk.Frame(tabs)
+tabs.add(setupTab, text = "Setup")
+tabs.pack(expand = 1, fill = "both")
+win.mainloop()
