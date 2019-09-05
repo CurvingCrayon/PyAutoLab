d45f862ee91a340ab09c2eca89bb285498e064cb
Hayden Keers <13200377@student.uts.edu.au>
2019-09-05 23:55:44 +1000

    Basic GUI

---
+++ gui.py
@@ -0,0 +1,6 @@
+import tkinter
+mainWindow = tkinter.Tk() 
+mainWindow.title("PyAutoLab")
+#create a title and display it with necessary space
+titleLb = tkinter.Label(mainWindow, text = "Hayden's PyAutoLab").pack() 
+mainWindow.mainloop()
