9ebec69942c142b3b1708cbc9746c6af5bc7d2bd
Hayden Keers <hayden.keers@gmail.com>
2019-09-06 12:02:03 +1000

    Detailed commandline stepping

--- getIDN.py
+++ getIDN.py
@@ -4,9 +4,9 @@ import basicIn as b
 rm = visa.ResourceManager()
 print("Connected instruments")
 print(rm.list_resources())
-#inst = rm.open_resource(rm.list_resources()[0])
-#print(inst.query("*IDN?"))
-#print(inst.write("C1: BSWV FRQ, 100"))
+inst = rm.open_resource(rm.list_resources()[0])
+print(inst.query("*IDN?"))
+print(inst.write("C1: BSWV FRQ, 123"))
 run = True
 while(run):
     start = b.getNum("Enter start frequency: ")
@@ -15,10 +15,10 @@ while(run):
     if  start and step and num:
         if start > 0:
             cycles = 10
-            for cyc in range(10):
-                freq = start + step * num
+            for cyc in range(num):
+                freq = start + step * cyc
                 print("Setting frequency: "+ str(freq))
-                #inst.write("C1: BSWV FRQ, "+str(freq))
+                inst.write("C1: BSWV FRQ, "+str(freq))
     else:
         print("Invalid num")
         run = False
